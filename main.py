72ca1fe7324ce42416ddebb73c32da15571b4eeb
oksana151277 <oksana.tihonova.1977@mail.ru>
2025-07-24 16:40:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ user_name = input("Как вас зовут?\n")
 user_points = 0
 
 print('\n'+user_name+', по какому предмету вы хотите пройти тест?')
-id_subj = input('1-Математика,\n2-Информатика,\n3-География,\n4-Физика,\n5-Химия,\n6-Биология,\n7-Русский язык,\n8-Литература,\n9-История,\n0-выход\n')
+id_subj = input('1-Математика,\n2-Информатика,\n3-География,\n4-Физика,\n5-Химия,\n6-Биология,\n7-Русский язык,\n8-Литература,\n9-Английский язык,\n0-выход\n')
 
 while id_subj != '0':
 
@@ -22,11 +22,32 @@ while id_subj != '0':
     elif id_subj == '6':
         test_math.test_math()
     elif id_subj == '7':
-        test_math.test_math()
+        local_points = test_school.test_school(questions_school.questions_russian)
+        normal_points = test_school.normal_school(local_points, questions_school.questions_russian)
+        print(f'\nТы набрал {local_points} баллов из {normal_points}')
+        if local_points/normal_points*100>=80:
+            print('Поздравляю, ты успешно прошел тестирование.\n')
+        else:
+            print('Нужно подтянуть знания. Возвращайся после повторения материала.\n')
+        user_points+=local_points
     elif id_subj == '8':
-        test_math.test_math()
+        local_points = test_school.test_school(questions_school.questions_literature)
+        normal_points = test_school.normal_school(local_points, questions_school.questions_literature)
+        print(f'\nТы набрал {local_points} баллов из {normal_points}')
+        if local_points/normal_points*100>=80:
+            print('Поздравляю, ты успешно прошел тестирование.\n')
+        else:
+            print('Нужно подтянуть знания. Возвращайся после повторения материала.\n')
+        user_points+=local_points
     elif id_subj == '9':
-        test_math.test_math()
+        local_points = test_school.test_school(questions_school.questions_english)
+        normal_points = test_school.normal_school(local_points, questions_school.questions_english)
+        print(f'\nТы набрал {local_points} баллов из {normal_points}')
+        if local_points/normal_points*100>=80:
+            print('Поздравляю, ты успешно прошел тестирование.\n')
+        else:
+            print('Нужно подтянуть знания. Возвращайся после повторения материала.\n')
+        user_points+=local_points
 
     print('\n'+user_name+', еще тест?')
     id_subj = input('1-Математика,\n2-Информатика,\n3-География,\n4-Физика,\n5-Химия,\n6-Биология,\n7-Русский язык,\n8-Литература,\n9-История\n0-выход\n')
