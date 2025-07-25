de3db085b4c0e38a219c6457db77a480e52f472f
annadymovaa <118061001+annadymovaa@users.noreply.github.com>
2025-07-25 20:41:35 +0500

    Add files via upload

---
+++ test_bio.py
@@ -0,0 +1,28 @@
+bio_question1 = '\nХлоропласты - органоиды, характерные для клеток:\n1)животных\n2)животных и растений\n3)растений\n4)человека#3'
+bio_question2 = '\nКакая эра является «Веком динозавров»?\n1)мезозойская\n2)протерозйская\n3)палеозойская\n4)кайнозойская#1'
+bio_question3 = '\nГен - это:\n1)материал для эволюционных процессов\n2)участок молекулы ДНК\n3)мономер белковой молекулы\n4)орган половой системы#2'
+bio_question4 = '\nКакие клетки участвуют в свертывании крови?#тромбоцит'
+
+def check_answer(q):
+    parce = q.find('#')
+    question = q[0:parce]
+    right_a = q[parce+1]
+    answer = input(question+'\nВведите ответ (только 1 правильный вариант):')
+    if right_a in answer:
+        return 1
+    else:
+        return 0
+def test_biology():
+    score = 0
+    print('\nТест по биологии:')
+    score += check_answer(bio_question1)
+    score += check_answer(bio_question2)
+    score += check_answer(bio_question3)
+    score += check_answer(bio_question4)
+    if score == 0:
+        print('\nВы провалили тест( Не быть вам биологом')
+    elif score == 4:
+        print('\nВашим знаниям позавидуют ученые! Поздравляем с успешным прохождением теста')
+    else:
+        print('\nВы немного разбираетесь, но сын маминой подруги знает биологию лучше. Возвращайтесь, когда подготовитесь.')
+
